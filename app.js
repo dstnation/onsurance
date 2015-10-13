66232d3f6e796163e16196b7e4e23165620a3799
teamDST <teamdst.dev@gmail.com>
2015-10-13 18:40:40 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var page = tabris.create("Page", {
+  topLevel: true,
+  title: "onsurance"
+});
+
+tabris.create("TextView", {
+  layoutData: {centerX: 0, centerY: 0},
+  text: "onsurance"
+}).appendTo(page);
